3a784a98c791648f4c0b87aee6634e2c3c7dc92d
Lori <57802869+loriZ77@users.noreply.github.com>
2021-01-26 23:08:39 -0800

    Initial commit

---
+++ src/test/java/com/example/wbdvsp2102yuyanzhouserverjava/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2102yuyanzhouserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
